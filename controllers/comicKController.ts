9e62dbf6d795533d40d1cd80f0be4234b48e08ef
volx <97158976+vo1x@users.noreply.github.com>
2024-07-31 19:23:38 +0545

    Refactored code to be more concise

--- controllers/comicKController.ts
+++ controllers/comicKController.ts
@@ -21,25 +21,23 @@ interface ChapterResult {
   groupName?: string;
 }
 
-const baseUrl = "https://api.comick.fun";
-const imgDomain = "http:192.168.1.80:5000";
-
-const agent = new https.Agent({
-  keepAlive: true,
-  maxVersion: "TLSv1.3",
-  minVersion: "TLSv1.3",
-});
+const config = {
+  baseUrl: "https://api.comick.fun",
+  imgDomain: "http://192.168.1.73:5000",
+  agent: new https.Agent({
+    keepAlive: true,
+    maxVersion: "TLSv1.3",
+    minVersion: "TLSv1.3",
+  }),
+};
 
 const api = axios.create({
-  httpsAgent: agent,
-  headers: {
-    "User-Agent": "Android",
-  },
+  httpsAgent: config.agent,
+  headers: { "User-Agent": "Android" },
 });
 
 const dateToHumanReadableForm = (timestamp: string): string => {
   const date = new Date(timestamp);
-
   const monthNames = [
     "January",
     "February",
@@ -54,52 +52,43 @@ const dateToHumanReadableForm = (timestamp: string): string => {
     "November",
     "December",
   ];
-
-  const year = date.getUTCFullYear();
-  const month = monthNames[date.getUTCMonth()];
-  const day = date.getUTCDate();
-
-  return `${month} ${day}, ${year}`;
+  return `${
+    monthNames[date.getUTCMonth()]
+  } ${date.getUTCDate()}, ${date.getUTCFullYear()}`;
 };
 
 const comicKController: ComicKController = {
   async getRoot(req, res) {
-    const { site } = req.query;
-    site && console.log(site);
     try {
       res.status(200).json({ message: "API is up" });
-    } catch (error: any) {
-      res.status(400).json({ message: error.message });
+    } catch (error) {
+      res.status(400).json({ message: "Failed to fetch API status" });
     }
   },
 
   async getSearch(req, res) {
     const { query } = req.query;
-    console.log(query);
-    const url = `${baseUrl}/v1.0/search?q=${query}?tachiyomi=true`;
-    const results: any = [];
+    if (!query) {
+      return res.status(400).json({ message: "Query parameter is required" });
+    }
+    const url = `${config.baseUrl}/v1.0/search?q=${query}&tachiyomi=true`;
     try {
       const { data } = await api.get(url);
-      console.log(data);
-      data.forEach((resultObj: any) => {
-        const result = {
-          source: "comick",
-          mangaID: resultObj.hid,
-          slug: resultObj.slug,
-          title: resultObj.title,
-          contentRating: resultObj.content_rating,
-          cover: {
-            width: resultObj.md_covers[0].w,
-            height: resultObj.md_covers[0].h,
-            url: imgDomain + "/image" + `/${resultObj.md_covers[0].b2key}`,
-          },
-        };
-        results.push(result);
-      });
-      console.log("search api called");
+      const results = data.map((resultObj: any) => ({
+        source: "comick",
+        mangaID: resultObj.hid,
+        slug: resultObj.slug,
+        title: resultObj.title,
+        contentRating: resultObj.content_rating,
+        cover: {
+          width: resultObj.md_covers[0].w,
+          height: resultObj.md_covers[0].h,
+          url: `${config.imgDomain}/image/${resultObj.md_covers[0].b2key}`,
+        },
+      }));
       res.status(200).json({ results });
-    } catch (error: any) {
-      res.status(400).json({ message: error.message });
+    } catch (error) {
+      res.status(400).json({ message: "Error fetching search results" });
     }
   },
 
@@ -107,39 +96,21 @@ const comicKController: ComicKController = {
     const { mangaID } = req.params;
     let page = 1;
     const chapters: ChapterResult[] = [];
-    const groupNames: string[] = [];
+    const groupNames: Set<string> = new Set();
 
     try {
-      console.log("get chapters called");
-
       while (true) {
-        const url = `${baseUrl}/comic/${mangaID}/chapters?lang=en&page=${page}&tachiyomi=true`;
-        console.log(`Fetching URL: ${url}`);
+        const url = `${config.baseUrl}/comic/${mangaID}/chapters?lang=en&page=${page}&tachiyomi=true`;
         const { data } = await api.get(url);
-
-        if (!data.chapters || data.chapters.length === 0) {
-          console.log("No more chapters to fetch");
-          break;
-        }
+        if (!data.chapters || data.chapters.length === 0) break;
 
         data.chapters.forEach((chapter: any) => {
-          console.log(chapter.group_name);
-          if (
-            Array.isArray(chapter.group_name) &&
-            chapter.group_name.length > 0
-          ) {
-            const groupNameToAdd = chapter.group_name
-              .pop()
-              .toLowerCase()
-              .trim();
-
-            if (!groupNames.includes(groupNameToAdd)) {
-              groupNames.push(groupNameToAdd);
-            }
-          } else {
-            console.error("chapter.group_name is not defined or not an array");
-          }
-          let chapterInfo: ChapterResult = {
+          const groupNameToAdd = chapter.group_name
+            ?.pop()
+            ?.toLowerCase()
+            .trim();
+          if (groupNameToAdd) groupNames.add(groupNameToAdd);
+          chapters.push({
             chId: chapter.hid,
             chNum: chapter.chap,
             title: chapter.title,
@@ -147,29 +118,23 @@ const comicKController: ComicKController = {
             language: chapter.lang,
             createdAt: dateToHumanReadableForm(chapter.created_at),
             isLastCh: chapter.is_the_last_chapter,
-            groupName: chapter.group_name,
-          };
-          chapters.push(chapterInfo);
+            groupName: groupNameToAdd,
+          });
         });
-
         page += 1;
       }
-
-      res.status(200).json({ chapters: chapters, groups: groupNames });
-    } catch (error: any) {
-      console.error("Error fetching chapters:", error.message);
-      res.status(400).json({ error: error.message });
+      res.status(200).json({ chapters, groups: Array.from(groupNames) });
+    } catch (error) {
+      res.status(400).json({ message: "Error fetching chapters" });
     }
   },
 
   async getMetadata(req, res) {
     const { mangaID } = req.params;
-    const url = `https://api.comick.fun/comic/${mangaID}?tachiyomi=true`;
-    console.log(url);
+    const url = `${config.baseUrl}/comic/${mangaID}?tachiyomi=true`;
     try {
       const { data } = await api.get(url);
-      console.log("manga metadata api called");
-      const mangaDetails: any = {
+      const mangaDetails = {
         source: "comick",
         mangaID: data.comic.hid,
         slug: data.comic.slug,
@@ -180,49 +145,29 @@ const comicKController: ComicKController = {
         cover: {
           width: data.comic.md_covers[0].w,
           height: data.comic.md_covers[0].h,
-          url: imgDomain + "/image" + `/${data.comic.md_covers[0].b2key}`,
+          url: `${config.imgDomain}/image/${data.comic.md_covers[0].b2key}`,
         },
-        authors: data.authors.map((author: { name: string }) => author.name),
+        authors: data.authors.map((author: any) => author.name),
       };
-
       res.status(200).json(mangaDetails);
-    } catch (error: any) {
-      res.status(400).json({ error: error.message });
+    } catch (error) {
+      res.status(400).json({ message: "Error fetching metadata" });
     }
   },
 
   async getPages(req, res) {
     const { chapterID } = req.params;
-    const url = `${baseUrl}/chapter/${chapterID}/get_images?tachiyomi=true`;
-
+    const url = `${config.baseUrl}/chapter/${chapterID}/get_images?tachiyomi=true`;
     try {
       const { data } = await api.get(url);
-      console.log("get pages called");
-
-      const pages = data.map(
-        (
-          image: {
-            h: number;
-            w: number;
-            name: string;
-            s: number;
-            b2key: string;
-          },
-          index: number
-        ) => ({
-          pgNum: image.b2key.split("-")[0],
-          url: imgDomain + "/image" + `/${image.b2key}`,
-          size: image.s,
-        })
-      );
-
+      const pages = data.map((image: any) => ({
+        pgNum: image.b2key.split("-")[0],
+        url: `${config.imgDomain}/image/${image.b2key}`,
+        size: image.s,
+      }));
       res.status(200).json({ pages });
     } catch (error) {
-      console.error(
-        `Error fetching manga chapter pages for ${chapterID}:`,
-        error
-      );
-      res.status(500).json({ error: "Internal Server Error" });
+      res.status(500).json({ message: "Internal Server Error" });
     }
   },
 };
--- routes/api.ts
+++ routes/api.ts
@@ -1,8 +1,8 @@
 import express from "express";
-import mediaController from "../controllers/mediaController";
+// import mediaController from "../controllers/mediaController";
 import comicKController from "../controllers/comicKController";
 const router = express.Router();
-import limiter from "../middleware/rateLimit";
+// import limiter from "../middleware/rateLimit";
 const { getRoot, getSearch, getChapters, getMetadata, getPages } =
   comicKController;
 
